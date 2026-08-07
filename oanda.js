81f504ee720c06a408ae188e7ae5b2a2dade2ea9
joethill <joe_t_hill@yahoo.com>
2026-08-07 12:50:17 -0400

    Update oanda.js

--- oanda.js
+++ oanda.js
@@ -1,14 +1,18 @@
-// Forex Scout V3 - OANDA Live Connector
+// Forex Scout V3 - OANDA Live Connection
 
 const OANDA_URL = "https://api-fxpractice.oanda.com";
 
 let apiToken = "";
 let accountID = "";
 
+
 function setCredentials(token, account){
+
     apiToken = token;
     accountID = account;
+
 }
+
 
 
 async function getPrice(pair){
@@ -16,33 +20,43 @@ async function getPrice(pair){
     try {
 
         const response = await fetch(
+
         `${OANDA_URL}/v3/accounts/${accountID}/pricing?instruments=${pair}`,
+
         {
-            headers:{
-                "Authorization": "Bearer " + apiToken,
-                "Content-Type": "application/json"
+
+            headers: {
+
+                "Authorization":
+                "Bearer " + apiToken,
+
+                "Content-Type":
+                "application/json"
+
             }
+
         });
+
 
         const data = await response.json();
 
+
         if(data.prices){
 
-            return {
-                pair: pair,
-                bid: data.prices[0].closeoutBid,
-                ask: data.prices[0].closeoutAsk
-            };
+            return data.prices[0].closeoutAsk;
 
         }
 
+
         return null;
+
 
     }
 
     catch(error){
 
         console.log(error);
+
         return null;
 
     }
